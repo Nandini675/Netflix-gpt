bd89e4010ca447350627951142dbbc5f556dea2d
Nandini675 <nandinisharma.5698@gmail.com>
2024-07-14 20:49:24 +0530

    installing taiwing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App  text-center font-mono  text-3xl my-36 p-9 py-16">
+      Helloo Everyone  lets build our Netflixgpt
     </div>
   );
 }
