d10ccfe64b9533a1dca316242ae17c197e41963b
Nandini675 <nandinisharma.5698@gmail.com>
2024-07-28 07:34:11 +0530

    deploy

--- src/components/GptSearchbar.js
+++ src/components/GptSearchbar.js
@@ -14,7 +14,7 @@ const dispatch=useDispatch();
  // search movie in tmdb
 
   const searchmovietmdb = async(movie) =>{
-     const data = await fetch('https://api.themoviedb.org/3/search/movie?query='+movie+'&include_adult=false&language=en-US&page=1',API_OPTIONS);
+     const data = await fetch(' https://api.themoviedb.org/3/search/movie?query='+movie+'&include_adult=false&language=en-US&page=1',API_OPTIONS);
 
      const json =  await data.json()
      return json.results;
